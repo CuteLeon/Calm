594409994188a11ddadc4773127539834f5a55cf
Leon <Leon@LEONVO>
2016-12-30 14:29:13 +0800

    首次提交

---
+++ Calm/MainForm.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.CloseButton = New System.Windows.Forms.Label()
+        Me.TitleLabel = New System.Windows.Forms.Label()
+        Me.AuthorLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'CloseButton
+        '
+        Me.CloseButton.BackColor = System.Drawing.Color.Transparent
+        Me.CloseButton.Image = Global.Calm.My.Resources.UnityResource.Close_0
+        Me.CloseButton.Location = New System.Drawing.Point(528, 10)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(64, 64)
+        Me.CloseButton.TabIndex = 0
+        Me.CloseButton.Tag = "Close"
+        '
+        'TitleLabel
+        '
+        Me.TitleLabel.BackColor = System.Drawing.Color.Transparent
+        Me.TitleLabel.Font = New System.Drawing.Font("微软雅黑", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TitleLabel.ForeColor = System.Drawing.Color.White
+        Me.TitleLabel.Image = Global.Calm.My.Resources.UnityResource.Favicon
+        Me.TitleLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.TitleLabel.Location = New System.Drawing.Point(20, 10)
+        Me.TitleLabel.Name = "TitleLabel"
+        Me.TitleLabel.Size = New System.Drawing.Size(298, 64)
+        Me.TitleLabel.TabIndex = 1
+        Me.TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'AuthorLabel
+        '
+        Me.AuthorLabel.BackColor = System.Drawing.Color.Transparent
+        Me.AuthorLabel.Font = New System.Drawing.Font("微软雅黑", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.AuthorLabel.ForeColor = System.Drawing.Color.White
+        Me.AuthorLabel.Location = New System.Drawing.Point(68, 151)
+        Me.AuthorLabel.Name = "AuthorLabel"
+        Me.AuthorLabel.Size = New System.Drawing.Size(307, 92)
+        Me.AuthorLabel.TabIndex = 2
+        Me.AuthorLabel.Text = "Leon"
+        Me.AuthorLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Calm.My.Resources.UnityResource.HomePage
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(601, 364)
+        Me.Controls.Add(Me.AuthorLabel)
+        Me.Controls.Add(Me.TitleLabel)
+        Me.Controls.Add(Me.CloseButton)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "MainForm"
+        Me.Text = "Clam · Leon"
+        Me.TopMost = True
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CloseButton As Label
+    Friend WithEvents TitleLabel As Label
+    Public WithEvents AuthorLabel As Label
+End Class
---
+++ Calm/MainForm.vb
@@ -0,0 +1,137 @@
+﻿Imports System.IO
+
+Public Class MainForm
+    ''' <summary>
+    ''' 标签间边距
+    ''' </summary>
+    Dim PaddingSize As Integer = 10
+    ''' <summary>
+    ''' 首页列表行列规模
+    ''' </summary>
+    Dim TabelSize As Size = New Size(5, 6)
+    ''' <summary>
+    ''' 放置标签的区域
+    ''' </summary>
+    Dim TabelRectangle As Rectangle
+
+#Region "窗体事件"
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Icon = My.Resources.UnityResource.Calm
+        CloseButton.Location = New Point(Me.Width - CloseButton.Width - 10, 10)
+        TitleLabel.Location = New Point(15, 10)
+        Dim Zoom As Double = Math.Min((Me.Width * 0.9 - PaddingSize * (TabelSize.Width - 1)) / TabelSize.Width / 640,
+                                      (Me.Height - TitleLabel.Bottom - 60) * 0.9 - PaddingSize * (TabelSize.Height - 1) / TabelSize.Height / 220)
+        AuthorLabel.Size = New Size(640 * Zoom, 220 * Zoom)
+        TabelRectangle.Size = New Size(TabelSize.Width * (AuthorLabel.Width + PaddingSize) - PaddingSize, TabelSize.Height * (AuthorLabel.Height + PaddingSize) - PaddingSize)
+        TabelRectangle.Location = New Point((Me.Width - TabelRectangle.Width) / 2, TitleLabel.Bottom + (Me.Height - TitleLabel.Bottom - 60 - TabelRectangle.Height) / 2)
+
+        AuthorLabel.Image = New Bitmap(My.Resources.UnityResource.AuthorLabel, AuthorLabel.Size)
+
+        GC.Collect()
+    End Sub
+
+    Private Sub MainForm_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        Me.TopMost = True
+    End Sub
+
+    Private Sub MainForm_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        LoadResource()
+    End Sub
+
+#End Region
+
+#Region "按钮动态效果"
+
+    Private Sub Button_MouseDown(sender As Label, e As MouseEventArgs) Handles CloseButton.MouseDown
+        sender.Image = My.Resources.UnityResource.ResourceManager.GetObject(sender.Tag & "_2")
+    End Sub
+
+    Private Sub Button_MouseEnter(sender As Label, e As EventArgs) Handles CloseButton.MouseEnter
+        sender.Image = My.Resources.UnityResource.ResourceManager.GetObject(sender.Tag & "_1")
+    End Sub
+
+    Private Sub Button_MouseLeave(sender As Label, e As EventArgs) Handles CloseButton.MouseLeave
+        sender.Image = My.Resources.UnityResource.ResourceManager.GetObject(sender.Tag & "_0")
+    End Sub
+
+    Private Sub Button_MouseUp(sender As Label, e As MouseEventArgs) Handles CloseButton.MouseUp
+        sender.Image = My.Resources.UnityResource.ResourceManager.GetObject(sender.Tag & "_1")
+    End Sub
+#End Region
+
+#Region "功能函数"
+
+    Private Sub LoadResource()
+        Dim RootDirectory As String = Application.StartupPath & "\Calm"
+        Dim CalmDirectory As String() = Directory.GetDirectories(RootDirectory)
+        Dim X As Integer = TabelRectangle.Left, Y As Integer = TabelRectangle.Top
+        For Index As Integer = 0 To CalmDirectory.Count - 1
+            Dim CalmLabel As Label = New Label
+            With CalmLabel
+                .Tag = (CalmDirectory(Index))
+                .BackColor = AuthorLabel.BackColor
+                .Font = AuthorLabel.Font
+                .ForeColor = AuthorLabel.ForeColor
+                .Size = AuthorLabel.Size
+                .Location = New Point(X, Y)
+                .TextAlign = AuthorLabel.TextAlign
+                .ImageAlign = AuthorLabel.ImageAlign
+                .Text = New DirectoryInfo(CalmDirectory(Index)).Name
+                .Image = New Bitmap(Bitmap.FromFile(CalmDirectory(Index) & "\" & .Text & ".jpg"), .Size)
+                AddHandler .MouseEnter, AddressOf Label_MouseEnter
+                AddHandler .MouseDown, AddressOf Label_MouseDown
+                AddHandler .MouseLeave, AddressOf Label_MouseLeave
+                .Show()
+            End With
+            Me.Controls.Add(CalmLabel)
+            If (Index Mod TabelSize.Width = 4) Then
+                X = TabelRectangle.Left
+                Y += AuthorLabel.Height + PaddingSize
+            Else
+                X += AuthorLabel.Width + PaddingSize
+            End If
+        Next
+        AuthorLabel.Location = New Point(X, Y)
+    End Sub
+
+#End Region
+
+#Region "控件事件"
+
+    Private Sub TitleLabel_MouseEnter(sender As Object, e As EventArgs) Handles TitleLabel.MouseEnter
+        Me.BackgroundImage = My.Resources.UnityResource.HomePage
+    End Sub
+
+    Private Sub CloseButton_Click(sender As Object, e As EventArgs) Handles CloseButton.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub AuthorLabel_Click(sender As Object, e As EventArgs) Handles AuthorLabel.Click
+        MsgBox("TODO:此处提倡装逼...")
+    End Sub
+
+#End Region
+
+#Region "标签事件"
+
+    Private Sub Label_MouseEnter(sender As Label, e As EventArgs) Handles AuthorLabel.MouseEnter
+        sender.BorderStyle = BorderStyle.FixedSingle
+        Me.BackgroundImage = New Bitmap(sender.Image)
+        GC.Collect()
+    End Sub
+
+    Private Sub Label_MouseDown(sender As Label, e As MouseEventArgs) Handles AuthorLabel.MouseDown
+        sender.Location = New Point(sender.Location.X + 2, sender.Location.Y + 2)
+        Me.Refresh()
+        '
+        sender.Location = New Point(sender.Location.X - 2, sender.Location.Y - 2)
+    End Sub
+
+    Private Sub Label_MouseLeave(sender As Label, e As EventArgs) Handles AuthorLabel.MouseLeave
+        sender.BorderStyle = BorderStyle.None
+    End Sub
+
+#End Region
+
+End Class
